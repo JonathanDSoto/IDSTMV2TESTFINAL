27ef3cd0d018a4d350be84b68a7f89e2e6c9eef9
Jonathan Soto <37603928+JonathanDSoto@users.noreply.github.com>
2026-02-19 12:38:43 -0700

    mi primera ventana

---
+++ src/main/Ventana.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+
+	public Ventana() {
+		
+		this.setVisible(true);
+		this.setSize(500, 500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//this.setLocationRelativeTo(null);
+		this.setMinimumSize(new Dimension(200,200));
+		this.setMaximumSize(new Dimension(800,800));
+		this.setTitle("Hola");
+		this.setBackground(Color.BLACK); 
+		this.setLocation(200, 200);
+		
+	}
+
+}
