e039fefa7a81036d3436d0ae64d532f007a96ce5
Jonathan Soto <jsoto1429@gmail.com>
2026-04-17 14:16:05 -0700

    conexión a base de datos

--- src/models/AuthModel.java
+++ src/models/AuthModel.java
@@ -1,5 +1,11 @@
 package models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.swing.JOptionPane;
 
 public class AuthModel {
@@ -10,17 +16,48 @@ public class AuthModel {
 	
 	public boolean login(String email,String password) {
 		
-		if(email.equals("fake@mail.com") ) {
-			if(password.equals("12345")) {
-				
-				return true; 
-				
-			}else {
-				return false; 
-			}
-		}else {
-			return false; 
+		
+		String query = "SELECT * FROM users WHERE email = ? AND password = ?";
+		
+		System.out.println(query);
+		
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+		    conn = DriverManager.getConnection(
+		        "jdbc:mysql://127.0.0.1:3306/application_db",
+		        "root",
+		        "root"
+		    );
+
+		    PreparedStatement ps = conn.prepareStatement(query);
+		    ps.setString(1, email);
+		    ps.setString(2, password);
+
+		    ResultSet rs = ps.executeQuery();
+			
+		    if (rs.next()) {
+		    	 
+		        return true;
+		    }  
+			
+		    rs.close();
+		    ps.close();
+		    conn.close();
+		    
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch (Exception e) {}
 		}
+		
+		return false; 
 		
 		 
 	}
