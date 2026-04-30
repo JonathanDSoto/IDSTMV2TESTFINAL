ba00b39eb8b2aeb30a8d3b0b8223beb774548c37
Jonathan Soto <37603928+JonathanDSoto@users.noreply.github.com>
2026-04-30 12:01:39 -0700

    ds store and mvc 1 login y registro

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 
+import controllers.AuthController;
+
 public class Main {
 
 	public static void main(String[] args) { 
 		
-		Ventana miventana = new Ventana();
+		//Ventana miventana = new Ventana();
 		
+		AuthController ac = new AuthController();
+		ac.showLogin();
 	}
 
 }
